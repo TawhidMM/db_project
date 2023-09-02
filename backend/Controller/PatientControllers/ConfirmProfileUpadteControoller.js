8db23a318842357f4e53195e48384363c1a164a7
Tawhid Mubashwir <135539633+TawhidMM@users.noreply.github.com>
2023-09-03 01:08:24 +0600

    patient edit profile pass bug fixed

--- backend/Controller/PatientControllers/ConfirmProfileUpadteControoller.js
+++ backend/Controller/PatientControllers/ConfirmProfileUpadteControoller.js
@@ -11,15 +11,21 @@ async function saveUpdates(req, res) {
 
     const addressId = `GET_ADD_ID('${STREET_ADDRESS}', '${CITY}', 
                             '${POSTAL_CODE}', '${SUB_DISTRICT}', '${DISTRICT}')`
-    const passwordHash = await hash(PASSWORD)
+
+    let setPassword = ''
+
+    if(PASSWORD.length !== 0){
+        const passwordHash = await hash(PASSWORD)
+        setPassword = `,PASSWORD = '${passwordHash}'`
+    }
 
     const query = `UPDATE PATIENT
                           SET
                           FIRST_NAME = '${FIRST_NAME}',
                           LAST_NAME = '${LAST_NAME}',
                           EMAIL = '${EMAIL}',
-                          ADDRESS_ID = ${addressId},
-                          PASSWORD = '${passwordHash}'
+                          ADDRESS_ID = ${addressId}
+                          ${setPassword}
                           WHERE PATIENT_ID = '${patientId}' `
 
     try {
